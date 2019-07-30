995f8e03b3536ae1915e1d2b1ca351444f7b4f12
Brittany Hartmire and TJ Epperson <bkhartmire+tj.epperson070@gmail.com>
2019-07-30 17:26:09 +0900

    Implemented AllPhotos

--- src/components/AllPhotos.jsx
+++ src/components/AllPhotos.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import _ from "lodash";
+import SinglePhoto from "./SinglePhoto";
+import { getSingleObject } from "../utils";
 
-export default class AllPhotos extends Component {
+export default class AllPhotos extends React.Component {
   constructor(props) {
+    console.log(props);
     super(props);
+    console.log(props);
   }
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>hi</h1>
+        {this.props.photos.map(photo => {
+          console.log(photo.Key);
+          return <SinglePhoto photoKey={photo.Key} />;
+        })}
+      </div>
+    );
   }
 }
