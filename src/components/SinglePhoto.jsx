66f3e2f11b87f4d7dd476544864740cb01dd69fb
Brittany Hartmire and TJ Epperson <bkhartmire+tj.epperson070@gmail.com>
2019-07-30 17:26:42 +0900

    Implemented rendering

--- src/components/SinglePhoto.jsx
+++ src/components/SinglePhoto.jsx
@@ -1,1 +1,30 @@
 import React from "react";
+import { getSingleObject } from "../utils/index";
+
+export default class SinglePhoto extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      base64: "",
+      done: false
+    };
+  }
+
+  componentDidMount() {
+    getSingleObject(this.props.photoKey).then(res =>
+      this.setState({ done: true, base64: res })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <img
+          src={`data:image/png;base64, ${this.state.base64}`}
+          alt={this.props.photoKey}
+        />
+      </div>
+    );
+  }
+}
